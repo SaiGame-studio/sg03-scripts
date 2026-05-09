74f64abe7f3c2cbec40c1ac9a346fe5abfc16318
Simon Sai <tranminhduc18116@gmail.com>
2026-05-10 06:59:13 +0700

    omega planing

--- Assets/SaiGame/LuaScript/Scripts/alpha_turn_end.lua
+++ Assets/SaiGame/LuaScript/Scripts/alpha_turn_end.lua
@@ -39,6 +39,10 @@ local function main()
     state.omega_front_line = o_front
     state.omega_back_line  = o_back
     state.omega_hand       = o_hand
+    
+    -- ── Omega attack planning ─────────────────────────────────────────────────
+    local attack_plan_err = lib_battle_ai.omega_try_to_attack(state)
+    if attack_plan_err ~= nil then output.error = attack_plan_err ; return end
 
     -- ── Advance turn ──────────────────────────────────────────────────────
     if state.metadata == nil then state.metadata = {} end
